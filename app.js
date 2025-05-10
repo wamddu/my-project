88e030760f60f574e94f15d5f142265dbf0f8e05
wamddu <yhy6923@naver.com>
2025-05-10 16:31:44 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log('서버가 http://localhost:${PORT}에서 실행 중입니다!');
+});
